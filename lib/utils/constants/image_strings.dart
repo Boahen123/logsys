30c0be2ff43d86b90079145c289a41fe5b9d3602
Papa Kofi <humblepapa1234@gmail.com>
2024-01-19 20:53:05 +0000

    defining all image strings in the app

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+// Strings that represent the directory to images
+
+///  directory to  image  on register screen.
+const String registerImage = 'assets/images/register.svg';
+
+///  directory to  image  on login screen.
+const String loginImage = 'assets/images/register.svg';
+
+///  directory to  image  on home screen.
+const String homeImage = 'assets/images/register.svg';
