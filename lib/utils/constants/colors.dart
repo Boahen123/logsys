f02fece68415681572325fc85ebca852b911d056
Papa Kofi <humblepapa1234@gmail.com>
2024-01-19 20:52:22 +0000

    file relocation within project directory

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Colors for the app
+Color appcolor1 = const Color(0xFF0071A4);
+Color appcolor2 = const Color(0xFFD9D9D9);
+Color appcolor3 = Colors.white;
