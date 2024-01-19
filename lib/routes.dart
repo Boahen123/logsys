af9e9a7f6f7ca0220c81384bcc7d77f2ea1b3547
Papa Kofi <humblepapa1234@gmail.com>
2024-01-19 20:16:12 +0000

    app routes defined

---
+++ lib/routes.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:logsys/views/login/login_screen.dart';
+import 'package:logsys/views/register/register_screen.dart';
+
+/// A list of routes for the application using the GetX package. Each
+/// route is represented by a `GetPage` object, representing the app's main routes.
+final List<GetPage<dynamic>> appRoutes = [
+  GetPage(name: '/login', page: () => const LoginScreen()),
+  GetPage(name: '/register', page: () => const RegisterScreen()),
+  // GetPage(name: '/home', page: () => const OTPScreen()),
+];
