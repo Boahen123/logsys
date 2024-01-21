7f498e3567aaa0b2f64daa88d909a820c9abe5b5
Papa Kofi <humblepapa1234@gmail.com>
2024-01-21 17:46:01 +0000

    all methods documented

--- lib/models/login_controller.dart
+++ lib/models/login_controller.dart
@@ -10,19 +10,43 @@ class LoginController extends GetxController {
   final phoneController = TextEditingController();
   final passwordController = TextEditingController();
 
+  /// The phoneValidator function checks if a given value is a valid phone number with 10 digits.
+  ///
+  /// Args:
+  ///   value (String): The value parameter is a nullable String that represents the phone number to be
+  /// validated.
+  ///
+  /// Returns:
+  ///   a string value. If the length of the input value is less than 10, it will return the message
+  /// "Please enter 10 digits." Otherwise, it will return null.
   String? phoneValidator(String? value) {
     return value!.length < 10 ? 'Please enter 10 digits.' : null;
   }
 
+  /// The function `passwordValidator` checks if a password is at least 6 characters long and returns an
+  /// error message if it is not.
+  ///
+  /// Args:
+  ///   value (String): The value parameter is a nullable String that represents the password to be
+  /// validated.
+  ///
+  /// Returns:
+  ///   a string. If the value is null or has a length less than 6, it returns the message "Password must
+  /// be at least 6 characters". Otherwise, it returns null.
   String? passwordValidator(String? value) {
     return (value == null || value.length < 6)
         ? 'Password must be at least 6 characters'
         : null;
   }
 
-  /// The function initiates phone authentication and navigates to the OTP screen.
-  void phoneAuthentication(String phoneNo) {}
-
+  /// The function encrypts a password using the SHA-256 hashing algorithm.
+  ///
+  /// Args:
+  ///   password (String): The `password` parameter is a string that represents the password that needs to
+  /// be encrypted.
+  ///
+  /// Returns:
+  ///   The encrypted password in the form of a string.
   String encryptPassword(String password) {
     final List<int> bytes = utf8.encode(password);
     final Digest hash = sha256.convert(bytes);
